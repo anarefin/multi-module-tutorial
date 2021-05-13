dfbef544d2ad8ae7f7ab1647f5133c92f0a08701
Arefin <arefin.sust@gmail.com>
2021-05-13 16:35:58 +0600

    Initial and Final commit

---
+++ main-application/src/main/java/com/boss/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.boss;
+
+import com.hmtmcse.tmutil.TomTom;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello, I am working!!!");
+        System.out.println("Random Integer: "+TomTom.randomInteger());
+    }
+}
